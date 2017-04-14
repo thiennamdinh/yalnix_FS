37492e4f408955120cb5b0ccdd23997f998a8748
kievking <ziqihong@ZIQIs-MacBook-Pro.local>
2017-04-13 23:24:02 -0500

    lru cache

--- lru_inode.c
+++ lru_inode.c
@@ -4,60 +4,61 @@
 #include <stdbool.h>
 
 #define SIZE 100
-// #define BLOCKSIZE 32
-// #define BLOCK_CACHESIZE 2 // Needs to be modified to another number.
+#define BLOCKSIZE 32
+#define BLOCK_CACHESIZE 2 // Needs to be modified to another number.
 int current_blockcache_number;
 
 
 //Dirty = 1 means it is dirty
 //Dirty = 0 means it is not dirty
 
-struct block_info {
+struct inode_info {
     int dirty;
-    int block_number;
-    struct block_info *next;
-    struct block_info *prev;
+    int inode_number;
+    struct inode_info *next;
+    struct inode_info *prev;
+    struct inode *inode_val; //From the filesystem.h
     char data[BLOCKSIZE];
 };
 
-struct block_wrap {
+struct inode_wrap {
    int key;
-   struct block_info* block_data;
+   struct inode_info* block_data;
 };
-struct block_info* front;
-struct block_info* rear;
+struct inode_info* front;
+struct inode_info* rear;
 
-struct block_wrap* block_hashtable[SIZE]; 
-struct block_wrap* default_block_wrap;
-struct block_info* default_block_info;
-struct block_wrap* item;
+struct inode_wrap* block_hashtable[SIZE]; 
+struct inode_wrap* default_inode_wrap;
+struct inode_info* default_inode_info;
+struct inode_wrap* item;
 
 int generate_hash_code(int key) {
    //Hash code generator.
    return key % SIZE;
 }
 
-struct block_wrap *get_block(int key) {
+struct inode_wrap *get_block(int key) {
    //Obtains the hash code.
    int hash_index = generate_hash_code(key);  
-	
+   
    while(block_hashtable[hash_index] != NULL) {
-	
+   
       if(block_hashtable[hash_index]->key == key){
          return block_hashtable[hash_index]; 
       }
-			
+         
       ++hash_index;
-		
+      
       hash_index %= SIZE;
    }        
-	
+   
    return NULL;        
 }
 
-void put_block_to_hashtable(int key, struct block_info* data_input) {
+void put_block_to_hashtable(int key, struct inode_info* data_input) {
 
-   struct block_wrap *item = (struct block_wrap*) malloc(sizeof(struct block_wrap));
+   struct inode_wrap *item = (struct inode_wrap*) malloc(sizeof(struct inode_wrap));
    item->block_data = data_input;
    item->key = key;
 
@@ -65,51 +66,51 @@ void put_block_to_hashtable(int key, struct block_info* data_input) {
 
    while(block_hashtable[hash_index] != NULL && block_hashtable[hash_index]->key != -1) {
       ++hash_index;
-		
+      
       hash_index %= SIZE;
    }
-	
+   
    block_hashtable[hash_index] = item;
 }
 
-struct block_wrap* remove_block_from_hashtable(int block_num) {
+struct inode_wrap* remove_block_from_hashtable(int inode_num) {
 
-   int hash_index = generate_hash_code(block_num);
+   int hash_index = generate_hash_code(inode_num);
 
    while(block_hashtable[hash_index] != NULL) {
-	
-      if(block_hashtable[hash_index]->key == block_num) {
-         struct block_wrap* temp = block_hashtable[hash_index]; 
-			
-         block_hashtable[hash_index] = default_block_wrap; 
+   
+      if(block_hashtable[hash_index]->key == inode_num) {
+         struct inode_wrap* temp = block_hashtable[hash_index]; 
+         
+         block_hashtable[hash_index] = default_inode_wrap; 
 
          return temp;
       }
-		
+      
       ++hash_index;
-		
+      
       hash_index %= SIZE;
    }      
-	
+   
    return NULL;        
 }
 
 void print_cache() {
    int i = 0;
-	
+   
    for(i = 0; i<SIZE; i++) {
-	
+   
       if(block_hashtable[i] != NULL)
          printf(" (%d,%d)",block_hashtable[i]->key,block_hashtable[i]->key);
       else
          printf(" ~~ ");
    }
-	
+   
    printf("\n");
 }
 
 
-void Enqueue_block(struct block_info * x) {
+void Enqueue_block(struct inode_info * x) {
    //Puts to end.
    if(front == NULL && rear == NULL){
       front = rear = x;
@@ -140,7 +141,7 @@ void Dequeue_block() {
 
 }
 
-void remove_queue(struct block_info * x) {
+void remove_queue(struct inode_info * x) {
    if(x->prev != NULL && x->next != NULL){
       x->prev->next = x->next;
       x->next->prev = x->prev;
@@ -153,9 +154,9 @@ void remove_queue(struct block_info * x) {
 }
 
 void Print() {
-   struct block_info* temp = front;
+   struct inode_info* temp = front;
    while(temp != NULL) {
-      printf("%d\n",temp->block_number);
+      printf("%d\n",temp->inode_number);
       temp = temp->next;
    }
    free(temp);
@@ -166,17 +167,17 @@ void init() {
    current_blockcache_number = 0;
    front = NULL;
    rear = NULL;
-   default_block_wrap = (struct block_wrap*) malloc(sizeof(struct block_wrap));
-   default_block_wrap->key = -1;
+   default_inode_wrap = (struct inode_wrap*) malloc(sizeof(struct inode_wrap));
+   default_inode_wrap->key = -1;
 
-   default_block_info = (struct block_info*) malloc(sizeof(struct block_info));
-   default_block_info->block_number = -1;
+   default_inode_info = (struct inode_info*) malloc(sizeof(struct inode_info));
+   default_inode_info->inode_number = -1;
 }
 
-struct block_info* get_lru_block(int block_num) {
-   struct block_wrap* result = get_block(block_num);
+struct inode_info* get_lru_block(int inode_num) {
+   struct inode_wrap* result = get_block(inode_num);
    if(result == NULL) {
-      return default_block_info;
+      return default_inode_info;
    }else{
       //Recently used.
       remove_queue(result->block_data);
@@ -190,7 +191,7 @@ struct block_info* get_lru_block(int block_num) {
 void evict(){
    //Test whether there is a key to be evicted.
    if(current_blockcache_number == BLOCK_CACHESIZE) {
-      int to_be_removed_key = front->block_number;
+      int to_be_removed_key = front->inode_number;
       //Here should be another method sync to write inode back to the disk.
 
 
@@ -198,7 +199,7 @@ void evict(){
 
 
       if(front->dirty == 1) {
-         WriteSector(front->block_number, (void*)(front->data));
+         WriteSector(front->inode_number, (void*)(front->data));
       }
       Dequeue_block();
       remove_block_from_hashtable(to_be_removed_key);
@@ -207,13 +208,13 @@ void evict(){
    }
 }
 
-void set_lru_block(int block_num, struct block_info* input_block) {
-   if(get_block(block_num) == NULL) {
+void set_lru_block(int inode_num, struct inode_info* input_block) {
+   if(get_block(inode_num) == NULL) {
       // printf("Key not found\n");
       //Determines whether a key needs to be removed.
       evict();
       Enqueue_block(input_block);
-      put_block_to_hashtable(block_num, input_block);
+      put_block_to_hashtable(inode_num, input_block);
       current_blockcache_number++;
       return;
    }else{
@@ -227,20 +228,20 @@ void set_lru_block(int block_num, struct block_info* input_block) {
 int main() {
 
    init();
-   struct block_info *x1 = (struct block_info*) malloc(sizeof(struct block_info));
-   x1->block_number = 1;
+   struct inode_info *x1 = (struct inode_info*) malloc(sizeof(struct inode_info));
+   x1->inode_number = 1;
 
-   struct block_info *x2 = (struct block_info*) malloc(sizeof(struct block_info));
-   x2->block_number = 2;
+   struct inode_info *x2 = (struct inode_info*) malloc(sizeof(struct inode_info));
+   x2->inode_number = 2;
 
-   struct block_info *x3 = (struct block_info*) malloc(sizeof(struct block_info));
-   x3->block_number = 3;
+   struct inode_info *x3 = (struct inode_info*) malloc(sizeof(struct inode_info));
+   x3->inode_number = 3;
 
-   struct block_info *x4 = (struct block_info*) malloc(sizeof(struct block_info));
-   x4->block_number = 4;
+   struct inode_info *x4 = (struct inode_info*) malloc(sizeof(struct inode_info));
+   x4->inode_number = 4;
 
-   struct block_info *x5 = (struct block_info*) malloc(sizeof(struct block_info));
-   x5->block_number = 5;
+   struct inode_info *x5 = (struct inode_info*) malloc(sizeof(struct inode_info));
+   x5->inode_number = 5;
 
    set_lru_block(1, x1);
    set_lru_block(2, x2);
@@ -252,13 +253,13 @@ int main() {
    // print_cache();
    //Test case 0:
    printf("----test case 0----\n");
-   printf("%d\n", get_lru_block(1)->block_number);
+   printf("%d\n", get_lru_block(1)->inode_number);
    set_lru_block(3, x3);
-   printf("%d\n", get_lru_block(2)->block_number);
+   printf("%d\n", get_lru_block(2)->inode_number);
    set_lru_block(4, x4);
-   printf("%d\n", get_lru_block(1)->block_number);
-   printf("%d\n", get_lru_block(3)->block_number);
-   printf("%d\n", get_lru_block(4)->block_number);
+   printf("%d\n", get_lru_block(1)->inode_number);
+   printf("%d\n", get_lru_block(3)->inode_number);
+   printf("%d\n", get_lru_block(4)->inode_number);
    // Print();
 
 
